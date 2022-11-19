2a6a297e0f4013db5eeb402b2f93bc607673d314
kolyabik <nikolai.yalok@gmail.com>
2022-11-19 16:51:59 +0300

    add icons

--- src/main/java/com/example/kurs/HelloApplication.java
+++ src/main/java/com/example/kurs/HelloApplication.java
@@ -3,6 +3,7 @@ package com.example.kurs;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -20,6 +21,7 @@ public class HelloApplication extends Application {
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
+        stage.getIcons().add(new Image(getClass().getResourceAsStream("shop.png")));
         this.stage = stage;
     }
 
